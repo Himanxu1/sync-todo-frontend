4be94453f85e7f2546cdc707fb4465e6b5c44f97
himanxu1 <himanshu.singh@wherehouse.io>
2025-10-05 14:27:37 +0530

    feat:add analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,15 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 
+import { Analytics } from "@vercel/analytics/next";
+
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker.register("/sw.js").catch(console.error);
 }
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <Analytics />
     <App />
   </StrictMode>
 );
